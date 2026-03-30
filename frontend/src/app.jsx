b8bbf6352dd1fba2424ac5abb26a32214efe81cb
Tayte Choudhury <t8c@tamu.edu>
2026-03-29 19:02:05 -0500

    Added imports

---
+++ frontend/src/app.jsx
@@ -0,0 +1,9 @@
+import { Route, Routes, useLocation } from 'react-router-dom';
+import NavBar from './components/NavBar.jsx';
+import HomePage from './pages/HomePage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import CashierPage from './pages/CashierPage.jsx';
+import ManagerPage from './pages/ManagerPage.jsx';
+import SalesPage from './pages/SalesPage.jsx';
+import KioskPage from './pages/KioskPage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx'; 
